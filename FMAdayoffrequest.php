c303ba24e134d24cca50d5eae7fd5590240f44ba
chadb3 <chadb3@ksu.edu>
2016-03-19 17:30:11 -0500

    added readme and more work on webpage

--- FMAdayoffrequest.php
+++ FMAdayoffrequest.php
@@ -1,3 +1,24 @@
+<?php 
+$a_var="asd";
+
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('Db3.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+      
+   }
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,15 +31,45 @@
 			background-color:purple;
 			color:white;
 			text-align:center;
-			 padding:5.5px;			
+			padding:5.5px;			
 		}
 		 #side 	{
 					line-height:30px;
 					background-color:#eeeeee;
 					height:600px;
-					width:100px;
+					width:159px;
 					float:left;
+					padding:1px;
+					
+				}
+				#bottom_div{
+					background-color:purple;
+					color:white;
+					clear:both;
+					text-align:center;
 					padding:5px;
+					
+				}
+				#req
+				{
+					color:red;
+				}
+				ul
+				{
+				
+					color:blue;
+					list-style-type:none;
+					float:left;
+					text-align:left;
+					padding-left:0px;
+				}
+				
+				
+				
+				#body
+				{
+					float: left;
+					padding-left:5px;
 				}
 			
 		
@@ -26,13 +77,23 @@
 	</style>
 	
 	<h1 id="head">Day off request form!</h1>
-	<div id="side"></div>
+	<div id="side"> 
+		<ul>
+			<li>Home</li>
+			<li>View Days Off</li>
+		</ul>
+		
+	</div>
+	
+	
+	
+<!--Body of the html has all the form actions and button!-->
 <div id="body">
-	<form>
-		   Name:<input type="text" name="name"> <br>
-		   Phone:<input type="text" name="phone"><br>
+	<form action="<?php $_PHP_var ?>" method ="GET">
+		   Name:<input type="text" name="name" required> <br>
+		   Phone:<input type="text" name="phone" required><br>
 		   <!--Reason:<input type="dropdown" name="asdf">-->		
-	</form>
+	
 	Reason: <select name="reason">
 		<option value="medical">Medical</option>
 		<option value="dential">Dential</option>
@@ -43,7 +104,19 @@
 	<br>
 	<!--<form>Elaborate Reason:<input type="text" name="elab_reason"></form>-->
 	<button type="button">Submit</button>
+	</form>
+	<?php echo $a_var; ?>
+	
+	
+	
+	
 </div>
+<!--Bottom div with information -->
+<div id="bottom_div">
+<p>FMA day off requester web-software</p>
+</div>
+
+
 
 
 
@@ -55,3 +128,4 @@
 
 
 </html>
+
