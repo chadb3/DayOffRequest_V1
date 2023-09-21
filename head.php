9683d2edb8ccbcd93b74b9b2080cd1176b7dd979
Chad <chadb78999@gmail.com>
2023-09-21 07:15:04 -0500

    work and line to note

---
+++ head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+<title>Homepage: Day Off Requester</title>
+<link rel="stylesheet" href="style.css">
+<meta charset="UTF-8">
+<meta name="description" content="Employees can request days off here">
+<meta name="developer" content="Chad B">
+</head>
+<header>
+Welcome: Company Day off requester.
+</header>
+<nav><ul><li><a href="index.php">Home</a></li><li><a href="requestDayOff.php">Request Days Off</a></li><li><a href="manageDaysOff.php">Manage Days off</a></li></ul></nav>
--- index.php
+++ index.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<head>
-<title>Homepage: Day Off Requester</title>
-<link rel="stylesheet" href="style.css">
-<meta charset="UTF-8">
-<meta name="description" content="Employees can request days off here">
-<meta name="developer" content="Chad B">
-</head>
-<header>
-Welcome: Company Day off requester.
-</header>
+<?php
+include("head.php");
+?>
+<article>
+<h1>Hello World</h1>
+<p> THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG!</P>
+</article>
